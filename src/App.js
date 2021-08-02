a36c4e244bc5c2de0c6863fae16635ad62ea6cfd
Tushar <Tusharyadav889@gmail.com>
2021-08-02 18:41:38 +0530

    added and configure react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <Switch>
+      <Route exact path='/'>
+        This is home page.
+      </Route>
+
+      <Route exact path='/starred'>
+        This is starred page.
+      </Route>
+
+      <Route>
+        Error 404 : Page not found
+      </Route>
+    </Switch>
   );
 }
 
