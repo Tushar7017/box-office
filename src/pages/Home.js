b109fb02b6a25b2d5c41ab8d25af438008c4ed5f
Tushar <Tusharyadav889@gmail.com>
2021-08-03 11:08:38 +0530

    added search option

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,10 +6,13 @@ const Home = () => {
 
     const [input, setInput] = useState('');
     const [results, setResults] = useState(null);
+    const [searchOption, setSearchOption] = useState('shows');
+
+    const isShowsSearch = searchOption === 'shows';
 
     const onSearch = () => {
 
-        apiGet(`/search/shows?q=${input}`)
+        apiGet(`/search/${searchOption}?q=${input}`)
             .then(result => {
                 setResults(result);
             });
@@ -25,6 +28,11 @@ const Home = () => {
         }
     }
 
+    const onRadioChange = (ev) => {
+        setSearchOption(ev.target.value);
+    }
+
+
     const renderResults = () => {
 
         if (results && results.length === 0) {
@@ -32,17 +40,17 @@ const Home = () => {
         }
 
         if (results && results.length > 0) {
-            return (
-                <div>
-                    {
-                        results.map((item) => (
-                            <div key={item.show.id}>
-                                {item.show.name}
-                            </div>
-                        ))
-                    }
-                </div>
-            );
+            return results[0].show ?
+                results.map((item) => (
+                    <div key={item.show.id}>
+                        {item.show.name}
+                    </div>
+                )) :
+                results.map((item) => (
+                    <div key={item.person.id}>
+                        {item.person.name}
+                    </div>
+                ))
         }
 
         return null;
@@ -52,9 +60,37 @@ const Home = () => {
         <MainPageLayout>
             <input
                 type="text"
+                placeholder="Search"
                 onChange={onInputChange}
                 onKeyDown={onKeyDown}
                 value={input} />
+
+            <div>
+
+                <label htmlFor="shows-search">
+                    Shows
+                    <input
+                        id="shows-search"
+                        type="radio"
+                        value="shows"
+                        checked={isShowsSearch}
+                        onChange={onRadioChange}
+                    />
+                </label>
+
+                <label htmlFor="actor-search">
+                    Actors
+                    <input
+                        id="actor-search"
+                        type="radio"
+                        value="people"
+                        checked={!isShowsSearch}
+                        onChange={onRadioChange}
+                    />
+                </label>
+
+            </div>
+
             <button
                 type="button"
                 onClick={onSearch}>
